62b35a706824071fe1b96ffaae06659d6ca6be81
Andras Kerekes <andras.kerekes.work@gmail.com>
2015-05-28 16:46:30 -0400

    pin security-monkey version

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,5 @@
 default['security_monkey']['basedir'] = '/usr/local/src/secmonkey'
-default['security_monkey']['branch'] = 'master'
+default['security_monkey']['branch'] = 'v0.3.4'
 default['security_monkey']['dart_version'] = '1.8.5-1'
 
 # specify your own secret and salt
